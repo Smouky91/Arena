4eeb9f94015c175962974593a46c9375149fde54
Smouyk91 <jurajber@gmail.com>
2018-10-18 14:59:15 +0200

    1. verzia

---
+++ Arena/Arena/Kocka.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arena
+{
+    class Kocka
+    {
+        /// <summary>
+        /// Generator nahodnych cisel
+        /// </summary>
+        private Random random;
+        /// <summary>
+        /// Pocet stran kocky
+        /// </summary>
+        private int pocetStran;
+
+        public Kocka()
+        {
+            pocetStran = 6;
+            random = new Random();
+        }
+        public Kocka(int pocetStran)
+        {
+            this.pocetStran = pocetStran;
+            random = new Random();
+        }
+        /// <summary>
+        /// Sluzi na citanie poctu stien koncky
+        /// </summary>
+        /// <returns>pocet stien kocky</returns>
+        public int VraciaPocetStien()
+        {
+            return pocetStran;
+        }
+        /// <summary>
+        /// Hod kockou
+        /// </summary>
+        /// <returns>Vracia hodnotu od 1 po pocet stran kocky</returns>
+        public int Hod()
+        {
+           return  random.Next(1, pocetStran+1);
+        }
+        /// <summary>
+        /// Vracia textovu reprezentaciu kocky
+        /// </summary>
+        /// <returns>Tato kocka ma "pocetStran" stran.</returns>
+        public override string ToString()
+        {
+            return String.Format("Tato kocka ma {0} stran.", pocetStran);
+        }
+    }
+}
---
+++ Arena/Arena/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kocka kocka1 = new Kocka();
+            Console.WriteLine(kocka1);
+            for (int i = 0; i < 10; i++)
+                Console.Write(kocka1.Hod() + " ");
+            Console.WriteLine();
+            Kocka kocka2 = new Kocka(10);
+            Console.WriteLine(kocka2);
+            for (int i = 0; i < 10; i++)
+                Console.Write(kocka2.Hod() + " ");
+            Console.ReadKey();
+
+        }
+    }
+}
